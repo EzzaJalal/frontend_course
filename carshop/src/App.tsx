3c01b87b5635de4625f1d30220b12890d5533a87
Ezza Jalal <syedaezzajalal@gmail.com>
2025-04-15 04:56:20 +0300

    Adding additional features such as Add, Delete, and Edit Car

--- carshop/src/App.tsx
+++ carshop/src/App.tsx
@@ -1,11 +1,20 @@
 import Container from '@mui/material/Container';
 import CssBaseline from '@mui/material/CssBaseline';
 import CarList from './components/CarList';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+
 function App() {
   return (
     <Container maxWidth="lg">
-      <CarList/>
-      <CssBaseline/>
+      <AppBar position='static'>
+        <Toolbar>
+          <Typography variant="h6">Car Shop</Typography>
+        </Toolbar>
+      </AppBar>
+      <CarList />
+      <CssBaseline />
     </Container>
   )
 }
